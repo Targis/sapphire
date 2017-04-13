8fb7d06f81cb054f863353b9ba4c7d507764b088
Yuri Kozyk (work) <y.kozyk@gmail.com>
2017-04-13 16:45:16 +0300

    update

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,32 @@
 $(function() {
 
-	// Custom JS
+	(function(window){
+
+		// get vars
+		var searchEl = $("#input");
+		var labelEl = $("#label");
+
+		// register clicks and toggle classes
+		labelEl.click(function(){
+				if (searchEl.hasClass("focus")) {
+					searchEl.removeClass("focus");
+					labelEl.removeClass("active");
+				} else {
+					searchEl.addClass("focus");
+					labelEl.addClass("active");
+				}
+		});
+
+		// register clicks outisde search box, and toggle correct classes
+		$(document).click(function(e){
+				var clickedID = e.target.id;
+				if (clickedID != "search-terms" && clickedID != "search-label") {
+						if (searchEl.hasClass("focus")) {
+								searchEl.removeClass("focus");
+								labelEl.removeClass("active");
+						}
+				}
+		});
+	}(window));
 
 });
