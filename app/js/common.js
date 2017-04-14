f5bd2171e6b9d596f1855935aa63297e88d7ce60
Yuri Kozyk (work) <y.kozyk@gmail.com>
2017-04-14 11:54:35 +0300

    .

--- app/js/common.js
+++ app/js/common.js
@@ -1,31 +1,35 @@
-$(function() {
+$(document).ready(function() {
 
 	(function(window){
 
 		// get vars
 		var searchEl = $("#input");
 		var labelEl = $("#label");
+		// console.log(searchEl);
+		// console.log(labelEl);
 
 		// register clicks and toggle classes
 		labelEl.click(function(){
-				if (searchEl.hasClass("focus")) {
-					searchEl.removeClass("focus");
-					labelEl.removeClass("active");
-				} else {
-					searchEl.addClass("focus");
-					labelEl.addClass("active");
-				}
+			if (searchEl.hasClass('focus')) {
+				searchEl.removeClass('focus');
+				labelEl.removeClass('active');
+			} else {
+				searchEl.addClass('focus');
+				labelEl.addClass('active');
+			}
 		});
 
 		// register clicks outisde search box, and toggle correct classes
 		$(document).click(function(e){
-				var clickedID = e.target.id;
-				if (clickedID != "search-terms" && clickedID != "search-label") {
-						if (searchEl.hasClass("focus")) {
-								searchEl.removeClass("focus");
-								labelEl.removeClass("active");
-						}
+			var clickedID = e.target.id;
+			console.log(clickedID);
+			if (clickedID != 'search-terms' && clickedID != 'search-label') {
+				// console.log('hello');
+				if (searchEl.hasClass('focus')) {
+					searchEl.removeClass('focus');
+					labelEl.removeClass('active');
 				}
+			}
 		});
 	}(window));
 
